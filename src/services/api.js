ba3235c7e7729e31248642ae81aa45e96e4c3ed3
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-29 23:12:44 +0300

    fetch movies by title, id, genre, actor, and director

--- src/services/api.js
+++ src/services/api.js
@@ -5,3 +5,28 @@ const instance = axios.create({
 });
 
 export default instance;
+
+// GET movies by title
+export const getMoviesByTitle = (title) => {
+  return instance.get(`Title/${process.env.REACT_APP_API_KEY}/${title}`);
+};
+
+// GET movies by id
+export const getMoviesById = (id) => {
+  return instance.get(`Title/${process.env.REACT_APP_API_KEY}/${id}`);
+};
+
+// GET movies by genre
+export const getMoviesByGenre = (genre) => {
+  return instance.get(`Genre/${process.env.REACT_APP_API_KEY}/${genre}`);
+};
+
+// GET movies by actor
+export const getMoviesByActor = (actor) => {
+  return instance.get(`Actor/${process.env.REACT_APP_API_KEY}/${actor}`);
+};
+
+// GET movies by director
+export const getMoviesByDirector = (director) => {
+  return instance.get(`Director/${process.env.REACT_APP_API_KEY}/${director}`);
+};
